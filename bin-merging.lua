83cec1a0dc95c9d22fc52194771a7b889cac3bc2
ivan386 <ivan386@users.noreply.github.com>
2019-10-29 17:37:21 +0300

    optimizing and comment

--- bin-merging.lua
+++ bin-merging.lua
@@ -1,3 +1,80 @@
+
+
+function merge_part(streams_in, buffer_length)
+	local out_part
+	for _, stream in ipairs(streams_in) do
+		local in_part = stream:read(buffer_length)
+
+		if not out_part then
+			out_part = in_part -- просто копируем часть из первого файла
+		elseif in_part and #in_part > 0 then
+
+ 			if #out_part < #in_part then
+				out_part, in_part = in_part, out_part
+			end
+			
+			if out_part ~= in_part 	-- данные различаются
+				and in_part:find("[^\0]")	-- есть не пустые места в in_part
+				and out_part:find("\0", 1, true) -- есть пустые места в out_part
+			then 
+				local find_index = 1
+--[[
+```
+Функция `string.gsub` подходит для задачи так как найдёт кусочки заполненные нулями и поставит то что передано ей.
+```lua
+--]]
+				out_part = out_part:gsub("\0+", function(zero_string)
+
+					if #in_part < find_index then
+						return -- не на что менять
+					end
+--[[
+```
+`string.gsub` не передаёт позицию в которой был найдено совпадение. Поэтому делаем параллельный поиск позиции `zero_string` при помощи функции `string.find`. Достаточно найти первый нулевой байт.
+```lua
+--]]
+					local start_index = out_part:find("\0", find_index, true)
+					find_index = start_index + #zero_string
+
+--[[
+```
+Теперь если в `in_part` есть данные для `out_part` копируем их.
+```lua
+--]]
+					if #in_part >= start_index then
+						local end_index = start_index + #zero_string - 1
+--[[
+```
+Вырезаем из `in_part` часть соответствующую последовательности нулей.
+```lua
+--]]
+						local part = in_part:sub(start_index, end_index)
+
+						if (part:byte(1) ~= 0) or part:find("[^\0]") then
+--[[
+```
+В `part` есть данные.
+```lua
+--]]
+							if #part == #zero_string then
+								return part
+							else
+--[[
+```
+`part` оказался меньше чем последовательность нулей. Дополняем его ими.
+```lua
+--]]
+								return part..zero_string:sub(1, end_index - #in_part)
+							end
+						end
+					end
+				end)
+			end
+		end
+	end
+	return out_part
+end
+
 local out_file = table.remove(arg,1)
 local in_files = {table.unpack(arg)}
 local error_msg
@@ -28,44 +105,39 @@ for _, in_file in pairs(in_files) do
 	end
 end
 
-local stream_out = io.open(out_file, "wb")
+local stream_check = io.open(out_file, "rb")
+local stream_out
+if not stream_check then
+	stream_out = io.open(out_file, "wb")
+else
+	print("File exists. Run file check.")
+	
+end
 
 local bytes = 0
-local buffer_length = 500000
+local buffer_length = 4 * 1024
 while true do
-	local out_bytes
-	for _, stream in ipairs(streams_in) do
-		local readen_bytes = stream:read(buffer_length)
-		if not out_bytes then
-			out_bytes = readen_bytes
-		elseif readen_bytes then
- 			if #out_bytes < #readen_bytes then
-				out_bytes, readen_bytes = readen_bytes, out_bytes
-			end
-			
-			if out_bytes ~= readen_bytes and readen_bytes:find("[^\0]") and out_bytes:find("\0", 1, true) then 
-				local index = 1
-				out_bytes = out_bytes:gsub("\0+", function(str)
-					if not index then return end
-					index = out_bytes:find("\0", index, true)
-					index = index + #str
-					if #readen_bytes >= index - 1 then
-						local part = readen_bytes:sub(index - #str, index - 1)
-						if part ~= str then
-							return part
-						end
-					elseif #readen_bytes >= index - #str then
-						return readen_bytes:sub(index - #str, #readen_bytes)..str:sub(1, index - 1 - #readen_bytes)
-					end
-				end)
+	local out_bytes = merge_part(streams_in, buffer_length)
+	if (not out_bytes) or #out_bytes == 0 then
+		if stream_check then
+			local part = stream_check:read(1)
+			if part and #part > 0 then
+				print("part not match")
 			end
 		end
-	end
-	if (not out_bytes) or #out_bytes == 0 then
-		stream_out:close()
+		(stream_check or stream_out):close()
 		break
 	end
-	stream_out:write(out_bytes)
+	if stream_out then
+		stream_out:write(out_bytes)
+	elseif stream_check then
+		local part = stream_check:read(buffer_length)
+		if (not part) or part ~= out_bytes then
+			print("part not match")
+			break
+		end
+	end
+	
 	bytes = bytes + #out_bytes
 	io.stderr:write(bytes.."\r")
 end
